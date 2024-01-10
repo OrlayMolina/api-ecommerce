0c7000b9c8eedb68cac43221ddb6587d326e53fc
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2024-01-10 13:21:21 -0500

    Commit inicial.

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.status(200).json({
+        msg: 'Hola desde Express',
+        code: 'OK'
+    });
+});
+
 app.listen(port, (req, res) => {
     console.log(`Servidor corriendo en el puerto ${port}`);
 });
