0cab493045cf7aa2f5be1766de5f51b9183b69a8
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2024-01-10 14:10:06 -0500

    Se crean las rutas para el modelo de producto.

--- index.js
+++ index.js
@@ -15,6 +15,34 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/products', (req, res) => {
+    res.status(200).json({
+        msg: 'GET | Hola desde Products',
+        code: 'OK'
+    });
+});
+
+app.post('/products', (req, res) => {
+    res.status(200).json({
+        msg: 'POST | Hola desde Products',
+        code: 'OK'
+    });
+});
+
+app.put('/products', (req, res) => {
+    res.status(200).json({
+        msg: 'PUT | Hola desde Products',
+        code: 'OK'
+    });
+});
+
+app.delete('/products', (req, res) => {
+    res.status(200).json({
+        msg: 'DELETE | Hola desde Products',
+        code: 'OK'
+    });
+});
+
 app.listen(port, (req, res) => {
     console.log(`Servidor corriendo en el puerto ${port}`);
 });
