5d428840d1288618f867c408b84f129b3bb390f4
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2024-02-04 14:02:47 -0500

    Se  actualiza userController.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const dbConnection = require('./database/config.js');
 //Middleware's
 app.use(cors());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
